f910693e67e72003efb425099c12dfa7d5b4c9ec
Divyadharshini <your_email@example.com>
2026-06-13 15:58:16 +0530

    Add variables problems

---
+++ variables_prac.py
@@ -0,0 +1,40 @@
+# personal information card
+name = input("enter a name:")
+age =  int(input("enter a age:"))
+place = input("enter a place:")
+print("name:",name)
+print("age:",age)
+print("place:",place)
+
+# student details
+name = input("enter a name:")
+reg_num = int(input("enter reg_num:"))
+age = int(input("enter age:"))
+department = input("enter department:")
+native = input("enter native:")
+blood_group = input("enter blood_group:")
+print("name:",name)
+print("reg_num:",reg_num)
+print("age:",age)
+print("department:",department)
+print("native:",native)
+print("blood_group:",blood_group)
+
+# age after 5 years
+current_age = 21
+future_age = current_age +5
+print("current_age:",current_age)
+print("future_age:",future_age)
+
+# salary increment
+current_s = 50000
+increment = current_s * 20/100
+new_s = current_s + increment
+print("current_s:",current_s)
+print("increment:",increment)
+print("new_s:",new_s)
+
+# celsius - fahrenheit
+celsius = 40
+fahrenheit =  (celsius*9/5)+32
+print (fahrenheit)
